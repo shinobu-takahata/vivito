717c369c9b43908a5eaa8772572fd10193bd314e
shinobu-takahata <shinoshino2335@gmail.com>
2024-06-29 12:57:06 +0900

    ⚡️ React Queryの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
 import Login from "./components/Login";
@@ -12,41 +13,44 @@ import Countries from "./components/Countries";
 // import Certifications from './components/Certifications';
 // import Projects from './components/Projects';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Router>
-      <div>
-        {/* <nav>
-          <ul>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/education">Education</Link>
-            </li>
-            <li>
-              <Link to="/work-experience">Work Experience</Link>
-            </li>
-            <li>
-              <Link to="/skills">Skills</Link>
-            </li>
-            <li>
-              <Link to="/certifications">Certifications</Link>
-            </li>
-            <li>
-              <Link to="/projects">Projects</Link>
-            </li>
-          </ul>
-        </nav> */}
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route element={<PrivateRoute />}>
-            <Route path="/countries" element={<Countries />} />
-          </Route>
-          {/* <Route path="/register" element={<Register />} />
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          {/* <nav>
+            <ul>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link to="/education">Education</Link>
+              </li>
+              <li>
+                <Link to="/work-experience">Work Experience</Link>
+              </li>
+              <li>
+                <Link to="/skills">Skills</Link>
+              </li>
+              <li>
+                <Link to="/certifications">Certifications</Link>
+              </li>
+              <li>
+                <Link to="/projects">Projects</Link>
+              </li>
+            </ul>
+          </nav> */}
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route element={<PrivateRoute />}>
+              <Route path="/countries" element={<Countries />} />
+            </Route>
+            {/* <Route path="/register" element={<Register />} />
           <Route path="/password-reset" element={<PasswordReset />} />
           <Route path="/profile" element={<PrivateRoute component={Profile} />} />
           <Route path="/dashboard" element={<PrivateRoute component={Dashboard} />} />
@@ -55,9 +59,10 @@ function App() {
           <Route path="/skills" element={<PrivateRoute component={Skills} />} />
           <Route path="/certifications" element={<PrivateRoute component={Certifications} />} />
           <Route path="/projects" element={<PrivateRoute component={Projects} />} /> */}
-        </Routes>
-      </div>
-    </Router>
+          </Routes>
+        </div>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -1,26 +1,23 @@
-import { useEffect, useState } from "react";
 import { supabase } from "../supabaseClient";
+import { useQuery } from "@tanstack/react-query";
 interface Country {
   id: number;
   name: string;
 }
+async function fetchCountries() {
+  const { data, error } = await supabase.from("countries").select();
+  if (error) throw new Error(error.message);
+  return data as Country[];
+}
+
 export default function Countries() {
-  const [countries, setCountries] = useState<Country[]>([]);
-
-  useEffect(() => {
-    getCountries();
-  }, []);
-
-  async function getCountries() {
-    const { data } = await supabase.from("countries").select();
-    console.log(data);
-
-    setCountries(data as Country[]);
-  }
-
+  const { data: countries } = useQuery({
+    queryKey: ["countries"],
+    queryFn: fetchCountries,
+  });
   return (
     <ul>
-      {countries.map((country) => (
+      {countries?.map((country: Country) => (
         <li key={country.name}>{country.name}</li>
       ))}
     </ul>
