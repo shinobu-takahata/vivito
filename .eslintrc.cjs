1c38f375bc967d01b4e8227df263b4012982a381
shinobu-takahata <shinoshino2335@gmail.com>
2024-07-10 01:55:47 +0900

    🚨 ルールの上書き

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,8 @@ module.exports = {
     'plugin:prettier/recommended',
   ],
   rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
     'prettier/prettier': 'error',
   },
 };
